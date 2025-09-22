ff1d05381e8fa78957ccb73980dafd0f1473ec23
raeist <heyiamraeist@gmail.com>
2025-09-22 20:14:51 +0530

    Create __init__.py

---
+++ src/oscillate/filters/__init__.py
@@ -0,0 +1,14 @@
+from oscillate.filters.base import BaseFilter, FilterChain
+from oscillate.filters.bass import BassBoost
+from oscillate.filters.audio_8d import Audio8D
+from oscillate.filters.eq import Equalizer
+from oscillate.filters.nightcore import Nightcore
+
+__all__ = [
+    "BaseFilter",
+    "FilterChain",
+    "BassBoost",
+    "Audio8D",
+    "Equalizer",
+    "Nightcore",
+]
